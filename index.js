1aa424d64ffbcb092404ef4d645e26c129e7c41b
atul-gairola <atulgairola08@gmail.com>
2021-09-27 14:31:35 +0530

    feat: get translation from api

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const apiURL = "https://api.funtranslations.com/translate/minion.json";
 
-let loading = true;
+let loading = false;
 
+const convertButton = document.getElementById("convertButton");
+const inputText = document.getElementById("text");
 
+function constructURL(text) {
+  return `${apiURL}?text=${text}`;
+}
 
+convertButton.addEventListener("click", () => {
+  loading = true;
+  const { value } = inputText;
+  console.log(value);
+
+  fetch(constructURL(value))
+    .then((response) => response.json())
+    .then((data) => {
+      const { translated } = data.contents;
+      loading = false;
+    })
+    .catch((err) => {
+      console.log(err);
+      loading = false;
+    });
+});
